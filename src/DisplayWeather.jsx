f37e3c6b73141ad2ef4897dd66713b76a9b98dd8
Rijan Tiwari <reezantiwaree@gmail.com>
2024-05-07 06:39:15 +0545

    rotating sun

--- src/DisplayWeather.jsx
+++ src/DisplayWeather.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-
+import sun from "./images/Sun.svg";
 const DisplayWeather = ({ location, weather, temperature, windSpeed }) => {
   return (
     <div className="weather-container">
       <Header location={location} weather={weather} />
+      <img src={sun} alt="Example" className="rotating-sun" />
       <p>Location: {location}</p>
       <p>Weather: {weather}</p>
       <p>Temperature: {temperature}°C</p>
